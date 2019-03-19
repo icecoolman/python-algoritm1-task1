bc67db3895e3533d4bb7c74b7748d09192815ac5
icecoolman <47063956+icecoolman@users.noreply.github.com>
2019-03-19 11:45:47 +0300

    Update kilomerty.py

--- kilomerty.py
+++ kilomerty.py
@@ -1,5 +1,6 @@
 def odometer(oksana):
-    sum = 0 
-    for i in range(0,len(oksana),2):
-        sum = sum + oksana[i]
+    sum = 0
+    sum = oksana[0]*oksana[1]
+    for i in range(2,len(oksana)-1,2):
+        sum = sum + oksana[i]*(oksana[i+1]-oksana[i-1])
     return sum
